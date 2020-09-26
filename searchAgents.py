45bce7a3ac4a87e37533d78b080a68f9ab835a75
zihanunimelb <69852798+zihanunimelb@users.noreply.github.com>
2020-09-26 21:18:35 +1000

    update for submission

--- searchAgents.py
+++ searchAgents.py
@@ -11,7 +11,6 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
 """
 This file contains all of the agents that can be selected to control Pacman.  To
 select an agent, use the '-p' option when running pacman.py.  Arguments can be
