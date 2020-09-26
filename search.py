c7b7dd2d20c700e16eac3c438c8330fc9c70f738
zihanunimelb <69852798+zihanunimelb@users.noreply.github.com>
2020-09-26 21:17:59 +1000

    update for submission

--- search.py
+++ search.py
@@ -11,7 +11,6 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
 """
 In search.py, you will implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
